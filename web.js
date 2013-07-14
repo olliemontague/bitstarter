a52f0f8c9819c85d488b70c92ba0fc229eafc615
John Smith <example@stanford.edu>
2013-07-14 15:29:04 +0000

    Updated web.js to have some text for katie

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Katie! I have written this code by remotely accessing a linux machine and then pushed it onto an amazon web hosting service to deploy this text... next I will try to make something that looks like a web page ');
 });
 
 var port = process.env.PORT || 5000;
